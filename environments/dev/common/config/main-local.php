658718ca06333303b754ad1c364e9c79b731cf98
Chris Smith <chris@cgsmith.net>
2025-02-19 15:06:45 +0100

    add queue to env script for testing

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -13,6 +13,17 @@ return [
             'class' => \yii\queue\sync\Queue::class,
             'handle' => true, // whether tasks should be executed immediately
         ],
+        /*'queue' => [
+            'class' => \yii\queue\db\Queue::class,
+            'db' => 'db', // DB connection component or its config
+            'tableName' => '{{%queue}}', // Table name
+            'channel' => 'default', // Queue channel key
+            'mutex' => 'yii\mutex\MysqlMutex', // Mutex used to sync queries
+            'as log' => 'yii\queue\LogBehavior',
+            //'as deadLetterBehavior' => \common\behaviors\DeadLetterQueue::class,
+            'ttr' => 5 * 60, // Max time for anything job handling
+            'attempts' => 3, // Max number of attempts
+        ],*/
         'mailer' => [
             'class' => \yii\symfonymailer\Mailer::class,
             'viewPath' => '@common/mail',
